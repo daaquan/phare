9f32e33db5efbbda61510da4fa8e5501a6f6c3d5
daaquan <daaquan@gmail.com>
2023-01-05 00:55:28 +0900

    refactor: HttpKernel使い方変更 #6

---
+++ app/Http/Kernel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http;
+
+use Framework\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,22 @@ $app = new Application($basePath);
 
 /*
 |--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
+
+$app->singleton(
+    \Framework\Contracts\Http\Kernel::class,
+    \App\Http\Kernel::class
+);
+
+/*
+|--------------------------------------------------------------------------
 | Load The Application Routes
 |--------------------------------------------------------------------------
 |
