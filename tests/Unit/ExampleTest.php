52e359d52ea3cae59383d0c514ba61614ddb26a0
daaquan <daaquan@gmail.com>
2023-01-04 20:34:37 +0900

    Pestテストフレームワーク導入 #4

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,13 +1,5 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    public function test_unit_example()
-    {
-        $this->assertTrue(true);
-    }
-}
+test('unittest example', function (): void {
+    expect(true)->toBeTrue();
+});
