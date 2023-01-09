80d4e8a0ecb431cbd26a8e855887feda5b2c4735
daaquan <daaquan@gmail.com>
2023-01-10 00:09:41 +0900

    update framework

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -2,6 +2,6 @@
 
 it('test version api response and status code')
     ->get('/version')
-    ->assertHeader('X-APP-NAME', env('APP_NAME'))
-    ->assertSee(env('APP_VER'))
+    ->assertHeader('X-APP-NAME', 'Skeleton')
+    ->assertSee('dev')
     ->assertOk();
