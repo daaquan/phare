693279e2ac8556427119b085a77a2130c09a91f9
daaquan <daaquan@gmail.com>
2023-10-11 21:58:14 +0900

    ネームスペース更新

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Framework\Foundation\Application;
+use Framework\Foundation\Micro as Application;
 
 /*
 |--------------------------------------------------------------------------
