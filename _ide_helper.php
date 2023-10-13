8a5775f90064632a8d99999037a863b85e415b94
daaquan <daaquan@gmail.com>
2023-10-13 21:52:48 +0900

    _ide_helperにFacades追加

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,12 +1,31 @@
 <?php
 
 namespace {
-
     class App extends \Framework\Support\Facades\Application
     {
     }
 
+    class Auth extends \Framework\Support\Facades\Auth
+    {
+    }
+
     class Log extends \Framework\Support\Facades\Log
+    {
+    }
+
+    class Security extends \Framework\Support\Facades\Security
+    {
+    }
+
+    class DB extends \Framework\Support\Facades\DB
+    {
+    }
+
+    class Request extends \Framework\Support\Facades\Request
+    {
+    }
+
+    class Response extends \Framework\Support\Facades\Response
     {
     }
 }
