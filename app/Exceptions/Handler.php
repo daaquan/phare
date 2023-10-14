4cf9a1049c3efc02d8b9404d82f5742ec3f8c59c
shosen.cho <s_tyou@asobimo.com>
2023-10-15 06:00:02 +0900

    Add ExceptionHander

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions;
+
+use Framework\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+}
