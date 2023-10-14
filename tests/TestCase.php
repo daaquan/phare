d2ef1dcec7806bf1c896a7f42595951942877cd8
daaquan <daaquan@gmail.com>
2023-10-15 03:31:14 +0900

    TestcaseにLoadConfigurationをブートさせる

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -72,6 +72,7 @@ class TestCase extends BaseTestCase
         $this->app = $this->createApplication();
         $this->app->bootstrapWith([
             \Framework\Foundation\Bootstrap\LoadEnvironmentVariables::class,
+            \Framework\Foundation\Bootstrap\LoadConfiguration::class,
             \Framework\Foundation\Bootstrap\RegisterFacades::class,
             \Framework\Foundation\Bootstrap\RegisterProviders::class,
         ]);
