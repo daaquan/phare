0ef8215ee978d19f8b4c4121f49d704b9cedbdb6
shosen.cho <s_tyou@asobimo.com>
2023-10-15 01:08:56 +0900

    不要なconfig読み込みを削除

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,20 +21,6 @@ $app = new Application(
 
 /*
 |--------------------------------------------------------------------------
-| Register Config Files
-|--------------------------------------------------------------------------
-|
-| Now we will register the "app" configuration file. If the file exists in
-| your configuration directory it will be loaded; otherwise, we'll load
-| the default version. You may register other files below as needed.
-|
-*/
-
-$app->configure('app');
-//$app->configure('whitelist');
-
-/*
-|--------------------------------------------------------------------------
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
 |
