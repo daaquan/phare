ff9ba4e04edf06a0244c987d29e9b1f87043dbd9
daaquan <daaquan@gmail.com>
2023-10-30 03:09:02 +0900

    ユーザーリポジトリーをバンドする

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,5 +10,9 @@ class AppServiceProvider implements ServiceProviderInterface
 {
     public function register(Application|DiInterface $app): void
     {
+        $app->bind(
+            \App\Contracts\Repository\UserContract::class,
+            \App\Repository\UserRepository::class
+        );
     }
 }
