e0ccbc5495b453054d202e710353c7d34a7abc4a
daaquan <daaquan@gmail.com>
2023-10-30 02:35:25 +0900

    Add sample db

---
+++ database/migrations/2023_10_20_000000_create_game_user_table.sql
@@ -0,0 +1,16 @@
+-- ユーザー情報テーブル
+CREATE TABLE `users`
+(
+    `id`                bigint unsigned NOT NULL,
+    `device_id`         varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `name`              varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `email`             varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email_verified_at` datetime                                                               DEFAULT NULL,
+    `password`          varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `birthday`          date                                                                   DEFAULT NULL,
+    `created_at`        timestamp NULL DEFAULT NULL,
+    `updated_at`        timestamp NULL DEFAULT NULL,
+    `deleted_at`        timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
