a6a950cf53f85b8df3e9972d0641daabe27dff0c
daaquan <daaquan@gmail.com>
2023-11-01 10:51:39 +0900

    Add migrate command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     protected array $commands = [
         \App\Console\Commands\AboutCommand::class,
         \App\Console\Commands\KeyGenerateCommand::class,
+        \App\Console\Commands\Migrate\MigrateCommand::class,
     ];
 
     /**
