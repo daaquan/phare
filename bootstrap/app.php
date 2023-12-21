018f0fe84915b0ea6fbc2d1d750f9c9f6556bf75
daaquan <daaquan@gmail.com>
2023-12-21 16:43:16 +0900

    configをモジュールで分ける

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 |
 */
 
-$app = \App\ApplicationFactory::createApplication();
+$basePath = $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__);
+$module = $_ENV['APP_MODULE'] ?? getenv('APP_MODULE');
+$app = $module === 'api' ?
+    new \Phox\Foundation\Micro($basePath, $module) :
+    new \Phox\Foundation\Web($basePath, $module);
 
 /*
 |--------------------------------------------------------------------------
---
+++ config/gmtool.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'providers' => [
+        \Phox\Providers\TranslateProvider::class,
+        \Phox\Providers\BladeViewProvider::class,
+
+        \App\Providers\DebugProvider::class,
+    ],
+
+    'aliases' => [],
+];
