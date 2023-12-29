ad9745a3567802134661ce39d7f31b751e42c4a9
shosen.cho <s_tyou@asobimo.com>
2023-12-29 22:44:56 +0900

    Add modules confg

--- config/modules.php
+++ config/modules.php
@@ -1,19 +1,70 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| 起動モジュール
+|--------------------------------------------------------------------------
+|
+| 環境変数でモジュール名を指定します。複数のモジュールを併用し、同じ環境で動作させることができます。
+| 例えば、APIとGmTool(Web)の2つのモジュールを同じnginxで動作させる場合、.envは .env.api と
+| .env.gmtool の2つを用意する必要があり、configやrouteのキャッシュも別々に生成されます。
+|
+*/
+
 return [
+
     'api' => [
         'prefix' => 'api',
 
-        'providers' => [],
+        'route' => [
+            base_path('routes/api.php'),
+            base_path('routes/raid.php'),
+        ],
 
+        'url' => env('APP_API_URL', 'http://localhost'),
+
+        'providers' => [],
         'aliases' => [],
     ],
 
     'gmtool' => [
+        'route' => base_path('app/Http/Controllers/GmTool'),
+
         'providers' => [
             \Phox\Providers\BladeViewProvider::class,
             \Phox\Providers\TranslateProvider::class,
         ],
+
+        'url' => env('APP_GMTOOL_URL', 'http://localhost'),
+
+        'aliases' => [],
+    ],
+
+    'webview' => [
+        'prefix' => 'webview',
+
+        'route' => base_path('app/Http/Controllers/Webview'),
+
+        'providers' => [
+            \Phox\Providers\BladeViewProvider::class,
+            \Phox\Providers\TranslateProvider::class,
+        ],
+
+        'url' => env('APP_WEBVIEW_URL', 'http://localhost'),
+
+        'aliases' => [],
+    ],
+
+    'callback' => [
+        'prefix' => 'callback',
+
+        'route' => [
+            base_path('callbacks'),
+        ],
+
+        'providers' => [],
+
+        'url' => env('APP_CALLBACK_URL', 'http://localhost'),
 
         'aliases' => [],
     ],
