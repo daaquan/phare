b9ab1b3aaf12fd18068b319a0453d706ecc9cac4
daaquan <daaquan@gmail.com>
2023-12-29 23:42:45 +0900

    英語の言語パッケージ追加

---
+++ lang/en/pages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'index' => [
+        'title' => 'Window Mockup',
+        'description' => 'The window mockup shows a box that looks like an operating system window.',
+    ],
+];
