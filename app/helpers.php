7e57859fa06978fcc7bf5dab0e0ddff96e669eba
daaquan <daaquan@gmail.com>
2024-01-04 16:24:09 +0900

    remove helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,1 @@
 <?php
-
-function abort($message, Phox\Foundation\Http\ResponseStatusCode $code = \Phox\Foundation\Http\ResponseStatusCode::BAD_REQUEST)
-{
-    return response(['message' => $message], $code);
-}
-
-function view($path, array $params = [])
-{
-    app()['dispatcher']->setParam('bladeView', $path);
-
-    app()['view']?->setVars($params);
-
-    return app()['blade'];
-}
