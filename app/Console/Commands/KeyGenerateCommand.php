3d47e43cacef046f6a4f739e3fc6711a0650af65
shosen.cho <s_tyou@asobimo.com>
2024-01-05 13:55:34 +0900

    key:generate例外処理追加

--- app/Console/Commands/KeyGenerateCommand.php
+++ app/Console/Commands/KeyGenerateCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Phox\Console\Command;
+use Phox\Support\Facades\Log;
+use Random\RandomException;
 use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(name: 'key:generate', description: 'Generate the application key.')]
@@ -14,7 +16,16 @@ class KeyGenerateCommand extends Command
         $envContent = file_get_contents($envPath);
 
         if (preg_match('/^APP_KEY=.*$/m', $envContent, $matches)) {
-            $key = base64_encode(random_bytes(32));
+            try {
+                $key = base64_encode(random_bytes(32));
+            } catch (RandomException $e) {
+                Log::error($e->getMessage());
+
+                $this->output->writeError('Failed to generate application key.');
+
+                return;
+            }
+
             $envContent = str_replace($matches[0], 'APP_KEY=base64:' . $key, $envContent);
             file_put_contents($envPath, $envContent);
 
