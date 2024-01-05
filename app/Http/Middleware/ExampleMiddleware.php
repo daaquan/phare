cf7ca083cd475bd0afa839eaf5f9ce0055d45c15
shosen.cho <s_tyou@asobimo.com>
2024-01-05 13:56:53 +0900

    fix Middlewares

--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -2,23 +2,13 @@
 
 namespace App\Http\Middleware;
 
-use Phalcon\Events\Event;
 use Phalcon\Http\RequestInterface;
 use Phalcon\Http\ResponseInterface;
-use Phalcon\Mvc\Micro;
 use Phox\Contracts\Http\MiddlewareContract;
 use Phox\Foundation\Http\Concerns\BeforeMiddleware;
 
 class ExampleMiddleware extends MiddlewareContract implements BeforeMiddleware
 {
-    public function before(Event $event, Micro $app): bool
-    {
-        return true;
-    }
-
-    /**
-     * Handle an incoming request.
-     */
     public function handle(RequestInterface $request, ResponseInterface $response)
     {
         if ($request->getHeader('X-DEBUG-MODE')) {
