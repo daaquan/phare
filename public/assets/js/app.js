5499ea5243ee070d4c94a2e83c1247bf7745a8a7
daaquan <daaquan@gmail.com>
2024-04-06 17:33:54 +0900

    prevent resubmits

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,6 +10,7 @@
 // app.js
 
 // prevent double submit
+// https://stackoverflow.com/questions/2830542/prevent-double-submission-of-forms-in-jquery
 document.querySelectorAll('form').forEach(function (form) {
   form.addEventListener('submit', function (event) {
     var submitButton = form.querySelector('[type="submit"]');
@@ -20,9 +21,21 @@ document.querySelectorAll('form').forEach(function (form) {
 });
 
 // prevent resubmit on back button
+// https://stackoverflow.com/questions/45656405/browser-prevent-post-data-resubmit-on-refresh-using-only-javascript
 if (window.history.replaceState) {
   window.history.replaceState(null, null, window.location.href);
 }
+
+// disable back button
+// https://stackoverflow.com/questions/12381563/how-to-stop-browser-back-button-using-javascript
+history.pushState(null, document.title, location.href);
+window.addEventListener('popstate', function (event) {
+  history.pushState(null, document.title, location.href);
+});
+
+// prevent closing window
+// https://stackoverflow.com/questions/821011/prevent-a-webpage-from-navigating-away-using-javascript
+//window.onbeforeunload = function() { return "Your work will be lost."; };
 
 /***/ }),
 
