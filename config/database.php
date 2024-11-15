b0b4ea1001793f57f0bea81d37e390098652596f
daaquan <daaquan@gmail.com>
2024-11-15 23:47:17 +0900

    move models and fix db config

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'db' => [
             'driver' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
