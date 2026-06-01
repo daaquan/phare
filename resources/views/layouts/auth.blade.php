a4d7c441993c5798373db379845ead22053328f6
daaquan <daaquan@gmail.com>
2026-06-01 10:26:18 +0900

    refactor: update command imports, asset paths, and lock dependencies - Update console kernel to use short class references. - Adjust asset URLs to use the new `assets/` directory. - Lock `webpack` and related dependencies to specific versions in `package.json` and `package-lock.json`.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -14,7 +14,7 @@
 
 
   @cssbox
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
   <style> body { visibility: hidden; } </style>
   <noscript><style> body { visibility: visible; } </style></noscript>
 </head>
@@ -37,7 +37,7 @@
 
 @include('partials.flash-messages')
 
-<script type="application/javascript" src="{{ asset('js/app.js') }}"></script>
+<script type="application/javascript" src="{{ asset('assets/js/app.js') }}"></script>
 @jsbox
 
 <script>
